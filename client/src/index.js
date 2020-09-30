178dc9a0e8c33c45b5abae1af7e1b6c758452716
Manav Misra <manavm@visionwebsoft.com>
2020-09-29 19:00:13 -0500

    :heavy_plus_sign: Add typography
    Including '🧚🏽‍♂️ theme'

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Typography from "typography";
+
+import fairyGatesTheme from "typography-theme-fairy-gates";
+
 import App from "./App";
+
+const typography = new Typography(fairyGatesTheme);
+
+typography.injectStyles();
 
 ReactDOM.render(
   <React.StrictMode>
